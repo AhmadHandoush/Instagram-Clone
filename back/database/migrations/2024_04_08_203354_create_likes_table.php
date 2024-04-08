78d202b196509d4b6c8b609486bb7adb9a057803
hamada <hamadahandoush9991@gmail.com>
2024-04-08 23:05:02 +0200

    some changes

--- back/database/migrations/2024_04_08_203354_create_likes_table.php
+++ back/database/migrations/2024_04_08_203354_create_likes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user-id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
